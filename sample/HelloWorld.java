33a40a312df2607d5d5ba6d6bc5e12499116d194
MrHate <gydeng0@gmail.com>
2021-04-15 08:54:21 +0000

    indent

--- sample/HelloWorld.java
+++ sample/HelloWorld.java
@@ -1,44 +1,44 @@
 public class HelloWorld {
 
-    private int n = 18;
+  private int n = 18;
 
-    public static int add2(int a, int b) { return a + b; }
+  public static int add2(int a, int b) { return a + b; }
 
-    public int add3(int a, int b, int c) { return a + b + c; }
+  public int add3(int a, int b, int c) { return a + b + c; }
 
-    public float fadd2(float a, float b) {
-      return a + b;
+  public float fadd2(float a, float b) {
+    return a + b;
+  }
+
+  public double dadd2(double a, double b) {
+    return a + b;
+  }
+
+  public long ladd2(long a, long b) {
+    return a + b;
+  }
+
+  public int getn() {
+    return n;
+  }
+
+  public void setn(int a) {
+    n = a;
+  }
+
+  public int recursive_sum(int a) {
+    if (a == 0)
+      return 0;
+    return a + recursive_sum(a - 1);
+  }
+
+  public int loop_sum(int a) {
+    int sum = 0;
+    while(a>0) {
+      sum += a;
+      a -= 1;
     }
-
-    public double dadd2(double a, double b) {
-      return a + b;
-    }
-
-    public long ladd2(long a, long b) {
-      return a + b;
-    }
-
-    public int getn() {
-      return n;
-    }
-
-    public void setn(int a) {
-      n = a;
-    }
-
-    public int recursive_sum(int a) {
-      if (a == 0)
-        return 0;
-      return a + recursive_sum(a - 1);
-    }
-
-    public int loop_sum(int a) {
-      int sum = 0;
-      while(a>0) {
-        sum += a;
-        a -= 1;
-      }
-      return sum;
-    }
+    return sum;
+  }
 }
 
